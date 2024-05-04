4434592a81450c4da565f8c842d662b8c2b9b89a
daddyaezzy <pulumbu11@gmail.com>
2024-05-04 22:07:27 +0100

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useRef } from "react";
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSystem from "./components/SoundSystem";
+import WebgiViwer from "./components/WebgiViwer";
+import Loader from "./components/Loader";
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
 
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
   return (
     <div className="App">
-      Hello
+      {/* <Loader /> */}
+      <div id="content" ref={contentRef}>
+        <Nav />
+        <Jumbotron />
+        <SoundSystem />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViwer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
